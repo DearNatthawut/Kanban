e48aa6c9c2aecc936ed5303ed378d78f964e3aa1
DearNatthawut <davilbm_9@hotmail.com>
2016-07-01 00:28:27 +0700

    Revert "New requirement"

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,20 @@
 'use strict';
 
 angular.module('kanban', [
-
-    'ngRoute',
-    'ui.sortable',
-    'ui.bootstrap',
-    'gantt', 'gantt.table',
-    'gantt.tooltips',
-    'xeditable'
-])
+        'ngRoute',
+        'ui.sortable',
+        'ui.bootstrap',
+        'gantt', 'gantt.table',
+        'gantt.tooltips',
+        'xeditable'
+    ])
 
     .config(['$compileProvider', function ($compileProvider) {
         $compileProvider.debugInfoEnabled(false); // testing issue #144
     }])
     .config(['$routeProvider', function ($routeProvider) {
 
-        $routeProvider.when('/', {templateUrl: '/views/kanban.html', controller: 'KanbanController'});
+        $routeProvider.when('/', {templateUrl: '/views/kanban.html'});
 
         $routeProvider.when('/kanban', {templateUrl: '/views/kanban.html', controller: 'KanbanController'});
 
--- resources/views/pages/board/edit.blade.php
+++ resources/views/pages/board/edit.blade.php
@@ -39,13 +39,6 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="worklimit" class="col-sm-2 control-label">Work limit</label>
-                                    <div class="col-sm-10">
-                                        <input type="number" id="worklimit" name="worklimit" class="form-control"
-                                               min="0" max="10" placeholder="Work limit" value="{{$Board->worklimit}}" />
-                                    </div>
-                                </div>
-                                <div class="form-group">
                                     <label for="reservation" class="col-sm-2 control-label">Estimate Date</label>
                                     <div class="col-sm-10 ">
                                         <input type="text" name="date" class="form-control " id="reservation"
